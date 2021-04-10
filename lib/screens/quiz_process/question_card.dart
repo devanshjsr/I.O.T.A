0fb81b531e97b115bab7c70a187751fbdf57136e
devanshjsr <devanshjsr@gmail.com>
2021-04-10 15:52:38 +0530

    Quiz can be attempted and Result displayed with UI

---
+++ lib/screens/quiz_process/question_card.dart
@@ -0,0 +1,250 @@
+import 'package:flutter/material.dart';
+
+import '../../styles.dart';
+import 'question_model.dart';
+
+class QuestionCard extends StatefulWidget {
+  final QuestionModel questionModel;
+  final int questionNumber;
+  final List<String> answerList;
+  final List<bool> markedForReview;
+
+  QuestionCard(this.questionModel, this.questionNumber, this.answerList,
+      this.markedForReview);
+  // QuestionCard({Key key}) : super(key: key);
+  @override
+  _QuestionCardState createState() => _QuestionCardState(
+      questionModel, questionNumber, answerList, markedForReview);
+}
+
+class _QuestionCardState extends State<QuestionCard>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  bool get wantKeepAlive => true;
+
+  QuestionModel questionModel;
+  int questionNumber;
+  List<String> answerList;
+  List<bool> markedForReview;
+
+  _QuestionCardState(this.questionModel, this.questionNumber, this.answerList,
+      this.markedForReview);
+
+  bool option1Selected,
+      option2Selected,
+      option3Selected,
+      option4Selected,
+      optionSelected;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    option1Selected = false;
+    option2Selected = false;
+    option3Selected = false;
+    option4Selected = false;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget getAnswerCard(bool isSelected, Function onPressed, String text) {
+      return Container(
+        margin: EdgeInsets.symmetric(vertical: 5),
+        width: double.infinity,
+        child: OutlinedButton(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.white,
+            side: BorderSide(
+                width: isSelected ? 4 : 2,
+                color: isSelected ? Colors.green : Colors.grey),
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          onPressed: () {
+            onPressed();
+          },
+          child: Text(
+            text,
+            style: TextStyle(
+                color: isSelected ? Colors.green : CustomStyle.primaryColor,
+                fontWeight: isSelected ? FontWeight.bold : FontWeight.normal),
+          ),
+        ),
+      );
+    }
+
+    super.build(context);
+    bool inputAllowed = true;
+    print(answerList);
+    print(markedForReview);
+
+    return Center(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+                side: BorderSide(
+                  width: 3,
+                  color: inputAllowed
+                      ? CustomStyle.primaryColor
+                      : Colors.green[800],
+                ),
+              ),
+              color: inputAllowed ? Colors.white : Colors.green[50],
+              elevation: 5,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      questionModel.question,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: CustomStyle.primaryColor,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    getAnswerCard(option1Selected, () {
+                      setState(() {
+                        option1Selected = true;
+                        option2Selected = false;
+                        option3Selected = false;
+                        option4Selected = false;
+                        answerList[questionNumber] = questionModel.option1;
+                      });
+                    }, questionModel.option1),
+                    getAnswerCard(option2Selected, () {
+                      setState(() {
+                        option1Selected = false;
+                        option2Selected = true;
+                        option3Selected = false;
+                        option4Selected = false;
+                        answerList[questionNumber] = questionModel.option2;
+                      });
+                    }, questionModel.option2),
+                    getAnswerCard(option3Selected, () {
+                      setState(() {
+                        option1Selected = false;
+                        option2Selected = false;
+                        option3Selected = true;
+                        option4Selected = false;
+                        answerList[questionNumber] = questionModel.option3;
+                      });
+                    }, questionModel.option3),
+                    getAnswerCard(option4Selected, () {
+                      setState(() {
+                        option1Selected = false;
+                        option2Selected = false;
+                        option3Selected = false;
+                        option4Selected = true;
+                        answerList[questionNumber] = questionModel.option4;
+                      });
+                    }, questionModel.option4),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 5),
+                      width: double.infinity,
+                      child: OutlinedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                          side: BorderSide(
+                              width: 3, color: CustomStyle.errorColor),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 30, vertical: 15),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            option1Selected = false;
+                            option2Selected = false;
+                            option3Selected = false;
+                            option4Selected = false;
+                            answerList[questionNumber] = null;
+                          });
+                        },
+                        child: Text(
+                          "Clear Selection",
+                          style: TextStyle(
+                            color: CustomStyle.errorColor,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 5),
+                      width: double.infinity,
+                      child: OutlinedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                          side: BorderSide(
+                              width: 3, color: CustomStyle.primaryColor),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 30, vertical: 15),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            markedForReview[questionNumber] = true;
+                          });
+                        },
+                        child: Text(
+                          "Mark for Review",
+                          style: TextStyle(
+                            color: CustomStyle.primaryColor,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 5),
+                      width: double.infinity,
+                      child: OutlinedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                          side: BorderSide(
+                              width: 3, color: CustomStyle.primaryColor),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 30, vertical: 15),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            markedForReview[questionNumber] = false;
+                          });
+                        },
+                        child: Text(
+                          "Unmark",
+                          style: TextStyle(
+                            color: CustomStyle.primaryColor,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/quiz_process/result_display.dart
@@ -0,0 +1,108 @@
+//import 'package:iota/screens/quiz_process/attempted_quizzes.dart';
+import 'package:iota/screens/quiz_process/show_quizzes.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+import '../../styles.dart';
+
+class ResultDisplay extends StatelessWidget {
+  final List<String> answerList;
+  final QuerySnapshot questionSnap;
+  BuildContext ctx;
+
+  ResultDisplay(this.answerList, this.questionSnap);
+
+  Future<bool> backCommand() {
+    Navigator.pushReplacement(
+        ctx, MaterialPageRoute(builder: (ctx) => ShowQuizzes()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ctx = context;
+    int totalQuestions = 0,
+        correctAnswers = 0,
+        incorrectAnswers = 0,
+        notAttempted = 0;
+    if (questionSnap == null) {
+      //print("ksks");
+    } else {
+      totalQuestions = questionSnap.docs.length;
+      for (int i = 0; i < totalQuestions; i++) {
+        List<String> tempList;
+        String temp = questionSnap.docs[i].data()["correctOption"];
+        //print(temp);
+        tempList = temp.split(" ");
+        String correctOptionIndex = (tempList[0] + tempList[1]).toLowerCase();
+        String correctOption = questionSnap.docs[i].data()[correctOptionIndex];
+        String optionSelected = answerList[i];
+
+        print(optionSelected);
+        print(correctOption);
+
+        if (optionSelected == null) {
+          notAttempted++;
+        } else if (optionSelected.compareTo(correctOption) == 0) {
+          correctAnswers++;
+        } else {
+          incorrectAnswers++;
+        }
+      }
+    }
+
+    Widget getResultTile(String title, String content, Color color) {
+      return Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+        padding: const EdgeInsets.fromLTRB(5, 10, 5, 5),
+        child: TextFormField(
+          readOnly: true,
+          initialValue: content,
+          textAlign: TextAlign.start,
+          decoration: InputDecoration(
+            fillColor: Colors.white,
+            filled: true,
+            contentPadding: EdgeInsets.all(16),
+            labelText: title,
+            labelStyle: TextStyle(
+              color: color,
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: color, width: 2),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            border: OutlineInputBorder(
+              borderSide: BorderSide(color: color, width: 2),
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+      );
+    }
+
+    return WillPopScope(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("QUIZ RESULT"),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                getResultTile("Total Questions", totalQuestions.toString(),
+                    CustomStyle.primaryColor),
+                getResultTile("Correct Responses", correctAnswers.toString(),
+                    Colors.green),
+                getResultTile("Incorrect Responses",
+                    incorrectAnswers.toString(), CustomStyle.errorColor),
+                getResultTile("Not Attempted", notAttempted.toString(),
+                    CustomStyle.primaryColor),
+              ],
+            ),
+          ),
+        ),
+        onWillPop: backCommand);
+  }
+}
